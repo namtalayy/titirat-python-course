e197e29e2d1cd5041065aa16376fb64f9ee01383
Titirat Prabpinart <titirat.p@ku.th>
2026-08-14 12:19:38 +0700

    edit currency

--- labs/week06-lab/example/test02.py
+++ labs/week06-lab/example/test02.py
@@ -1,6 +1,6 @@
 # Function
 # THB <-> USD .. 1 USD = 32 THB
-# THB <-> JYP .. 1 JYP = 22 THB
+# THB <-> JPY .. 1 JYP = 22 THB
 
 def convert_currency(amount, currency):
     if currency == "USD":
@@ -9,9 +9,9 @@ def convert_currency(amount, currency):
         print(f"{amount:.2f} USD = {amount * 32.0} THB")
     
     if currency == "JYP":
-        print(f"{amount:.2f} THB = {amount / 22.0} JYP")
+        print(f"{amount:.2f} THB = {amount / 22.0} JPY")
     else:
-        print(f"{amount:.2f} JYP = {amount * 22.0} THB")
+        print(f"{amount:.2f} JPY = {amount * 22.0} THB")
         
 convert_currency(100, "USD")
-convert_currency(100, "JYP")
+convert_currency(100, "JPY")
